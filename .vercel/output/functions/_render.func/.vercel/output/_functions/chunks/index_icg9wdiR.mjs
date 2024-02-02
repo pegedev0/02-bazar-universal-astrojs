621b5f5643a8a3d486327f03c950344779312aea
pegedev0 <pegedev0@gmail.com>
2024-02-02 14:27:08 +0100

    Configurando Netlify

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_icg9wdiR.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./prerender_EwNzhb1J.mjs').then(n => n.i);
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'hybrid',
-  adapter: vercel()
+  adapter: netlify()
 });
